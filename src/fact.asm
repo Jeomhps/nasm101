954f379570a4d16a81413482de20cc5dedcbbfe1
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2024-11-27 01:28:10 +0100

    feat : Added a recursive factorial implementation

---
+++ src/fact.asm
@@ -0,0 +1,46 @@
+section .data
+    newline db 0x0A    ; Newline character for cleaner output
+    msg_negative db '-', 0
+
+section .bss
+    tab resd 10
+    buffer resb 16
+
+section .text
+    global _start
+
+_start:
+    mov eax, 5
+    mov dword [tab], 1
+    mov ecx, tab
+
+    call factorial
+
+    jmp exit
+
+factorial:
+    ; base case
+    cmp eax, 1
+    je end_recursion
+
+    push eax
+    dec eax
+
+    call factorial
+
+    pop ebx
+    imul eax, ebx
+
+    add ecx,4
+    mov [ecx], eax
+
+    ret
+
+end_recursion:
+    mov eax, 1
+    ret
+
+exit:
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
